3723357e722f70d2d6337aa3443b2ba24ff414be
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-08 23:42:12 -0700

    Added users, email, and auth-management services

--- server/services/index.js
+++ server/services/index.js
@@ -1,3 +1,12 @@
+const users = require('./users/users.service.js');
+
+const email = require('./email/email.service.js');
+
+const authManagement = require('./auth-management/auth-management.service.js');
+
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
+  app.configure(users);
+  app.configure(email);
+  app.configure(authManagement);
 };
