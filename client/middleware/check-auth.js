e1e87ef6e230b75db48b915f23632aa8f356733a
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-09 20:30:37 -0700

    Added auth nuxt middleware

---
+++ client/middleware/check-auth.js
@@ -0,0 +1,12 @@
+// import parseCookies from '~/utils/parse-cookies';
+
+export default function ({ store, redirect }) {
+  // const accessToken = store.state.auth.accessToken;
+  // // console.log(accessToken);
+  // if (accessToken && !store.state.auth.user) {
+  //   console.log('Authenticating...');
+  //   store.dispatch('auth/authenticate', { strategy: 'jwt', accessToken });
+  // } else {
+  //   console.log('Something else happened');
+  // }
+}
