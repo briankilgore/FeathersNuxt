22c81cd56c285d1ad27ece0fee4b567edc3cbe7c
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-09 20:33:28 -0700

    Added notification wrapper component

---
+++ client/components/Notifications.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <no-ssr>
+      <notifications group="notice" width="100%" position="top center">
+        <template slot="body" scope="props">
+          <v-alert
+            class="alert ma-0"
+            :type="props.item.type"
+            transition="scale-transition"
+            :value="true"
+            v-on:input="props.close"
+            dismissible
+          >
+            {{ props.item.text }}
+          </v-alert>
+        </template>
+      </notifications>
+    </no-ssr>
+    <no-ssr>
+      <notifications  group="alert" position="bottom center">
+        <template slot="body" scope="props">
+          <v-snackbar
+            :timeout="timeout"
+            bottom
+            multi-line
+            :value="true"
+          >
+            {{ props.item.text }}
+            <v-btn flat color="pink" @click.native="props.close">Close</v-btn>
+          </v-snackbar>
+        </template>
+      </notifications>
+    </no-ssr>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      timeout: 6000,
+    };
+  },
+  computed: {
+    message: function () {
+      console.log(this.$store);
+      return this.$store.state.alerts.message;
+    },
+    display: {
+      get: function () {
+        console.log(this.$store.state.alerts.display);
+        return this.$store.state.alerts.display;
+      },
+      set: function (value) {
+        this.$store.dispatch('alerts/display', value);
+      },
+    },
+  },
+  methods: {
+    close: function () {
+      console.log('closing notifications');
+    },
+  },
+};
+</script>
+
+<style>
+  .alert {
+    position: absolute;
+    width: 100% !important;
+  }
+</style>
