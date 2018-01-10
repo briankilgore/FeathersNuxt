542ac51c1cce0ab46a9c3c5fe32b49c46c6d31b5
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-09 20:33:39 -0700

    Added Contact page

---
+++ client/pages/contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout
+      justify-center
+      align-center
+    >
+      <h1>Contact</h1>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'authenticated',
+};
+</script>
+
+<style>
+
+</style>
