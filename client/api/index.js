b0fb639502175758df5323e00bad567ccb04fec2
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-09 20:22:40 -0700

    Setting up feathers-vuex  - Added Feathers API client  - Initialized vuex store  - Added helpers for managing storage when rendered server side and for parsing cookies

---
+++ client/api/index.js
@@ -0,0 +1,15 @@
+import feathers from 'feathers/client';
+import auth from 'feathers-authentication-client';
+import hooks from 'feathers-hooks';
+import socketio from 'feathers-socketio/client';
+import feathersStorage from '~/utils/feathers-storage';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3030', {transports: ['websocket']});
+
+const api = feathers()
+  .configure(socketio(socket))
+  .configure(hooks())
+  .configure(auth({ storage: feathersStorage }));
+
+export default api;
---
+++ client/store/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'; // eslint-disable-line import/no-extraneous-dependencies
+import Vuex from 'vuex'; // eslint-disable-line import/no-extraneous-dependencies
+import feathersVuex, { initAuth } from 'feathers-vuex';
+import feathers from '~/api';
+// import parseCookies from '~/utils/parse-cookies';
+// import * as AlertStore from '~/store/alerts';
+
+Vue.use(Vuex);
+
+const { service, auth } = feathersVuex(feathers, { idField: '_id' });
+
+const store = new Vuex.Store({
+  modules: {
+    // alerts: {
+    //   namespaced: true,
+    //   ...AlertStore,
+    // },
+  },
+  plugins: [
+    // service('users'),
+    service('authManagement'),
+    auth({ userService: 'users' }),
+  ],
+  state: {},
+  actions: {
+    nuxtServerInit: function ({ commit, dispatch }, { req }) { // eslint-disable-line consistent-return
+      let auth = initAuth({
+        commit,
+        dispatch,
+        req,
+        moduleName: 'auth',
+        cookieName: 'feathers-jwt',
+      });
+
+      return auth;
+    },
+  },
+});
+
+export default () => store;
