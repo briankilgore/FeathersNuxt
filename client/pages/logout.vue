e50dd75d46efdbb9edc5567e77593645b1c760ef
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-09 20:34:10 -0700

    Added Logout page

---
+++ client/pages/logout.vue
@@ -0,0 +1,39 @@
+<template>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  data: function () {
+    return {
+      error: '',
+    };
+  },
+  methods: {
+    ...mapActions({
+      logout: 'auth/logout',
+    }),
+  },
+  mounted: function () {
+    console.log('Logging user out');
+    this.logout()
+      .then(() => {
+        this.$router.replace('/login');
+      });
+  },
+  watch: {
+    error: function () {
+      if (this.error) {
+        setTimeout(() => {
+          this.error = '';
+        }, 3000);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
+
