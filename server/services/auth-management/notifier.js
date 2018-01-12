05f9277927eac0917d03d3c1da970d56293f04a7
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-11 21:46:58 -0700

    Added support for sendResetPwd and passwordChange types to notifier

--- server/services/auth-management/notifier.js
+++ server/services/auth-management/notifier.js
@@ -20,12 +20,13 @@ module.exports = function(app) {
    * 'identityChange' - Sent when user changes identity (i.e. email address) info (from authManagement.identityChange API calls)
    */
   return function(type, user, notifierOptions) {
-    console.log(`-- Preparing email for ${type}`)
-
+    console.log(`-- Preparing email for ${type}`);
+    console.log(user);
+    let filename, data;
     switch (type) {
       case 'resendVerifySignup': // send an email with link for verifying user's email address
-        let filename = 'server/email-templates/verify.ejs';
-        let data = {
+        filename = 'server/email-templates/verify.ejs';
+        data = {
           user: user,
           verifyHref: `http://localhost:3030/verify?token=${user.verifyToken}`,
         };
@@ -37,15 +38,57 @@ module.exports = function(app) {
             from: 'noreply@example.com',
             to: user.email,
             subject: 'Thanks for Registering',
-            html: str
+            html: str,
          };
 
          return sendEmail(email);
         });
 
-        break
+        break;
+      case 'sendResetPwd': // send an email with link for resetting user's password
+        filename = 'server/email-templates/reset-password.ejs';
+        data = {
+          user: user,
+          verifyHref: `http://localhost:3030/reset-password?token=${user.resetToken}`,
+        };
+
+        ejs.renderFile(filename, data, function(err, str) {
+          // console.log('VERIFY EMAIL', err, str);
+
+          email = {
+            from: 'noreply@example.com',
+            to: user.email,
+            subject: 'Password reset request received',
+            html: str,
+         };
+
+         return sendEmail(email);
+        });
+
+        break;
+      case 'passwordChange': // send an email with link for resetting user's password
+        filename = 'server/email-templates/password-changed.ejs';
+        data = {
+          user: user,
+          verifyHref: `http://localhost:3030/reset-password?token=${user.resetToken}`,
+        };
+
+        ejs.renderFile(filename, data, function(err, str) {
+          // console.log('VERIFY EMAIL', err, str);
+
+          email = {
+            from: 'noreply@example.com',
+            to: user.email,
+            subject: 'Password changed',
+            html: str,
+         };
+
+         return sendEmail(email);
+        });
+
+        break;
       default:
-        break
+        break;
     }
   };
 };
