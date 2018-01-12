1557e1401139fea74cb516ab2286bb5e28fee368
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-11 21:24:40 -0700

    Updated reset-password page - Added background image - Improved notifications - Added transparent top bar with button

--- client/pages/reset-password.vue
+++ client/pages/reset-password.vue
@@ -1,27 +1,61 @@
 <template>
-  <v-container fluid fill-height>
+  <v-container class="test" fluid fill-height>
+    <v-toolbar color="transparent" absolute flat>
+      <v-toolbar-title><Logo height="36"></Logo></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn to="/login" flat>Login</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
     <v-layout
       justify-center
       align-center
     >
       <v-flex
-        xs6
+        xs4
       >
         <v-card>
-          <v-toolbar color="indigo" dark card flat>
-            <v-toolbar-side-icon v-if="$vuetify.breakpoint.width <= 1264" @click.stop="toggleDrawer"></v-toolbar-side-icon>
+          <v-toolbar color="green" dark card flat>
             <v-toolbar-title>Reset Password</v-toolbar-title>
           </v-toolbar>
-          <v-card-text>
-            <v-form v-model="valid" ref="form" lazy-validation>
+          <v-card-text class="pa-5">
+            <v-form v-if="!token" v-model="valid" ref="form" lazy-validation v-on:submit.prevent="requestPasswordReset">
               <v-text-field
                 label="Email"
-                v-model="emailAddress"
+                prepend-icon="email"
+                v-model="email"
                 :rules="emailRules"
                 required
               ></v-text-field>
+              <v-btn
+                :loading="$store.state.auth.isAuthenticatePending"
+                :disabled="$store.state.auth.isAuthenticatePending"
+                type="submit"
+                block
+              >
+                Submit
+              </v-btn>
             </v-form>
-            <v-btn @click="submit" block>submit</v-btn>
+            <v-form v-else v-model="valid" ref="form" lazy-validation v-on:submit.prevent="resetPassword">
+              <v-text-field
+                label="Password"
+                prepend-icon="lock"
+                v-model="password"
+                :rules="passwordRules"
+                :append-icon="showPassword ? 'visibility' : 'visibility_off'"
+                :append-icon-cb="() => (showPassword = !showPassword)"
+                :type="showPassword ? 'text' : 'password'"
+                required
+              ></v-text-field>
+              <v-btn
+                :loading="$store.state.auth.isAuthenticatePending"
+                :disabled="$store.state.auth.isAuthenticatePending"
+                type="submit"
+                block
+              >
+                Submit
+              </v-btn>
+            </v-form>
           </v-card-text>
         </v-card>
       </v-flex>
@@ -30,13 +64,14 @@
 </template>
 
 <script>
+import Logo from '~/components/Logo';
+import { mapActions } from 'vuex';
+
 export default {
   data: function () {
     return {
-      emailAddress: '',
+      email: '',
       password: '',
-      valid: false,
-      showPassword: false,
       emailRules: [
         (v) => !!v || 'E-mail is required',
         (v) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid',
@@ -44,21 +79,89 @@ export default {
       passwordRules: [
         (v) => !!v || 'Password is required',
       ],
+      showPassword: false,
+      valid: false,
     };
   },
+  computed: {
+    token: function () {
+      return this.$route.query.token;
+    },
+  },
   methods: {
-    submit: function () {
+    ...mapActions('authManagement', {
+      authManagementCreate: 'create',
+    }),
+    requestPasswordReset: async function () {
       console.log('Form submitted');
-      if (this.$refs.form.validate()) {
-        console.log('Form valid');
-      } else {
+      if (!this.$refs.form.validate()) {
         console.log('Form invalid');
+        return;
+      }
+      console.log(`Sending password reset request to ${this.email}`);
+      try {
+        // send signup verification notification
+        let response = await this.authManagementCreate({
+          action: 'sendResetPwd',
+          value: { email: this.email },
+        });
+        console.log(response);
+
+        this.$notify({
+          group: 'notice',
+          type: 'success',
+          text: `Password reset request sent. Please check your email.`,
+        });
+      } catch (error) {
+        console.log(error);
+        this.$notify({
+          group: 'notice',
+          type: 'error',
+          text: `Unable to reset password. ${error.message}`,
+        });
       }
     },
+    resetPassword: async function () {
+      console.log('Form submitted');
+      if (!this.$refs.form.validate()) {
+        console.log('Form invalid');
+        return;
+      }
+      console.log(`Resetting password...`);
+      try {
+        let response = await this.authManagementCreate({
+          action: 'resetPwdLong',
+          value: {
+            token: this.token, // compares to .resetToken
+            password: this.password, // new password
+          },
+        });
+        console.log(response);
+
+        this.$notify({
+          group: 'notice',
+          type: 'success',
+          text: `Password successfully reset. Please return to login screen to continue.`,
+        });
+      } catch (error) {
+        console.log(error);
+        this.$notify({
+          group: 'notice',
+          type: 'error',
+          text: `Unable to reset password. ${error.message}`,
+        });
+      }
+    },
+  },
+  components: {
+    Logo,
   },
 };
 </script>
 
-<style>
-
+<style scoped>
+.container {
+  background-image: url(/images/man-working-in-modern-office_4460x4460_bw.jpg);
+  background-size: cover;
+}
 </style>
