afab91b56ac0cde98819419518e481aad4f0c742
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-11 21:19:51 -0700

    Added Logo component

---
+++ client/components/Logo.vue
@@ -0,0 +1,19 @@
+<template>
+  <img v-bind:style="{ maxHeight: height + 'px' }" src="/images/feathers-logo-wide.png" />
+</template>
+
+<script>
+export default {
+  props: {
+    height: String,
+  },
+  data: function () {
+    return {
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
