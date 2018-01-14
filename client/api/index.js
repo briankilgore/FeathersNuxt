09d2738133a31c8c4f563c54f00323bbe6a58d3a
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-14 00:02:35 -0700

    Changed socketio init to use location.origin for host

--- client/api/index.js
+++ client/api/index.js
@@ -4,12 +4,15 @@ import hooks from 'feathers-hooks';
 import socketio from 'feathers-socketio/client';
 import feathersStorage from '~/helpers/feathers-storage';
 import io from 'socket.io-client';
+// import { CookieStorage } from 'cookie-storage';
 
-const socket = io('http://localhost:3030', {transports: ['websocket']});
+const host = location.origin;
+const socket = io(host, {transports: ['websocket']});
 
 const api = feathers()
   .configure(socketio(socket))
   .configure(hooks())
   .configure(auth({ storage: feathersStorage }));
+  // .configure(auth({ storage: new CookieStorage() }));
 
 export default api;
