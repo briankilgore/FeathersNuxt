c807ea15289637fa009192f00faa2a9b5850dab5
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-14 23:06:47 -0700

    Updated middleware to run async. If we err while authenticating, we logout and redirect user to login

--- client/middleware/check-auth.js
+++ client/middleware/check-auth.js
@@ -1,11 +1,20 @@
-// import parseCookies from '~/utils/parse-cookies';
+import storage from '~/helpers/feathers-storage';
 
-export default function ({ store, redirect }) {
-  const accessToken = store.state.auth.accessToken;
+
+export default async function ({ store, redirect }) {
+  console.log('check-auth');
+  const accessToken = storage.getItem('feathers-jwt');
   // console.log(accessToken);
-  if (accessToken && !store.state.auth.user) {
+  if (accessToken && !store.state.auth.payload) {
     console.log('Authenticating...');
-    store.dispatch('auth/authenticate', { strategy: 'jwt', accessToken });
+    await store.dispatch('auth/authenticate', { strategy: 'jwt', accessToken })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+        store.dispatch('auth/logout');
+      })
   } else {
     console.log('Something else happened');
   }
