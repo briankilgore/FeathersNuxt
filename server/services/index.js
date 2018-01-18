56910f7daeeff75498a070b82471c29f776c6132
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-18 07:40:43 -0700

    Registered upload service with server

--- server/services/index.js
+++ server/services/index.js
@@ -4,9 +4,12 @@ const email = require('./email/email.service.js');
 
 const authManagement = require('./auth-management/auth-management.service.js');
 
+const upload = require('./upload/upload.service.js');
+
 module.exports = function () {
   const app = this; // eslint-disable-line no-unused-vars
   app.configure(users);
   app.configure(email);
   app.configure(authManagement);
+  app.configure(upload);
 };
