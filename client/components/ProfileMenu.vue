12b012368b523eefcbad24ac93874dbe5851e9c7
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-22 12:03:59 -0700

    Moved Profile Menu from navigation menu to toolbar and updated style to match new location. Also, added currentUser prop to bring in the user's actual information instead of the placeholder info used while prototyping

--- client/components/ProfileMenu.vue
+++ client/components/ProfileMenu.vue
@@ -1,24 +1,45 @@
 <template>
   <v-menu offset-y>
-    <v-btn class="white--text" flat value="justify" block :ripple="false" slot="activator">
-      <span>Brian Kilgore</span>
-      <v-spacer></v-spacer>
-      <v-icon>arrow_drop_down</v-icon>
+    <v-btn class="white--text" flat fab :ripple="false" slot="activator">
+      <v-avatar size="40px">
+        <img :src="currentUser.profileUrl" />
+      </v-avatar>
     </v-btn>
-    <v-list dense>
-      <v-list-tile to="/profile" nuxt>
-        <v-list-tile-title>Profile</v-list-tile-title>
-      </v-list-tile>
-      <v-divider></v-divider>
-      <v-list-tile to="/logout" nuxt>
-        <v-list-tile-title>Logout</v-list-tile-title>
-      </v-list-tile>
-    </v-list>
+    <v-card>
+      <v-list dense>
+        <v-list-tile class="pt-3 pb-3" avatar>
+          <v-list-tile-avatar>
+            <img :src="currentUser.profileUrl" style="width: 48px; height: 48px;" />
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ currentUser.firstName }} {{ currentUser.lastName }}</v-list-tile-title>
+            <v-list-tile-sub-title>{{ currentUser.email }}</v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider></v-divider>
+        <v-list-tile to="/profile" nuxt>
+          <v-list-tile-avatar>
+            <v-icon left>settings</v-icon>
+          </v-list-tile-avatar>
+          <v-list-tile-content>
+            <v-list-tile-title class="body-2">Profile</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-divider></v-divider>
+      </v-list>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" flat to="/logout">Logout</v-btn>
+      </v-card-actions>
+    </v-card>
   </v-menu>
 </template>
 
 <script>
 export default {
+  props: {
+    currentUser: { type: Object, default: () => {} },
+  },
   methods: {
   },
 };
