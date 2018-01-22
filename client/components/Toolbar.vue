8dd22153a2363a0e9cbb544c41a1111aac785072
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-22 12:04:47 -0700

    Added Profile Menu to top Toolbar component. Also, added currentUser prop to pass into profile Menu component

--- client/components/Toolbar.vue
+++ client/components/Toolbar.vue
@@ -2,20 +2,23 @@
   <v-toolbar class="elevation-1" color="white" clipped-left fixed app>
     <v-toolbar-side-icon v-show="$vuetify.breakpoint.width <= 1264" @click.stop="toggleDrawer"></v-toolbar-side-icon>
     <v-toolbar-title><Logo height="24"></Logo></v-toolbar-title>
+    <v-spacer></v-spacer>
+    <profile-menu :currentUser="currentUser"></profile-menu>
   </v-toolbar>
 </template>
 
 <script>
 import Logo from '~/components/Logo';
+import ProfileMenu from '~/components/ProfileMenu';
 
 export default {
   props: {
     drawer: Boolean,
     title: String,
+    currentUser: { type: Object, default: () => {} },
   },
   data: function () {
     return {
-      breakpointWidth: 0,
     };
   },
   methods: {
@@ -25,6 +28,7 @@ export default {
   },
   components: {
     Logo,
+    ProfileMenu,
   },
 };
 </script>
