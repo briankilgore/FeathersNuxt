3aa51f0dc02548f4e25a50f67cf898336a30aa19
Brian Kilgore <brianleekilgore@gmail.com>
2018-01-29 22:55:07 -0700

    Added new menu items  - Availability  - Sessions  - Schedule  - Clients  - Admin Group (clients, locations, users, settings)

--- client/components/NavigationDrawer.vue
+++ client/components/NavigationDrawer.vue
@@ -6,7 +6,7 @@
     app
   >
     <v-card flat>
-      <v-list>
+      <v-list dense>
         <v-list-tile to="/" exact nuxt>
           <v-list-tile-action>
             <v-icon>home</v-icon>
@@ -15,22 +15,63 @@
             <v-list-tile-title>Home</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile to="/contacts" exact nuxt>
+        <v-list-tile to="/availability" exact nuxt>
           <v-list-tile-action>
-            <v-icon>contact_mail</v-icon>
+            <v-icon>event_available</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Contacts</v-list-tile-title>
+            <v-list-tile-title>Availability</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
-        <v-list-tile to="/admin" exact nuxt>
+        <v-list-tile to="/sessions" exact nuxt>
           <v-list-tile-action>
-            <v-icon>settings</v-icon>
+            <v-icon>schedule</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Administration</v-list-tile-title>
+            <v-list-tile-title>Sessions</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
+        <v-list-tile to="/clients" exact nuxt>
+          <v-list-tile-action>
+            <v-icon>perm_contact_calendar</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Clients</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-group group="admin">
+          <v-list-tile slot="item" nuxt>
+            <v-list-tile-action>
+              <v-icon>settings</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Administration</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-icon>keyboard_arrow_down</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+          <v-list-tile to="/admin/clients">
+            <v-list-tile-content>
+              <v-list-tile-title>Clients</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile to="/admin/locations">
+            <v-list-tile-content>
+              <v-list-tile-title>Locations</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile to="/admin/users">
+            <v-list-tile-content>
+              <v-list-tile-title>Users</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile to="/admin/settings">
+            <v-list-tile-content>
+              <v-list-tile-title>Settings</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list-group>
       </v-list>
     </v-card>
   </v-navigation-drawer>
@@ -65,7 +106,7 @@ export default {
   },
   components: {
     ProfileMenu,
-  }
+  },
 };
 </script>
 
